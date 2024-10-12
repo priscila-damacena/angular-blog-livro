4d51495e04e5eb0c2afd3a98c2e81a4b3d64adc3
priscila-damacena <priscila201705@gmail.com>
2024-10-12 12:52:14 -0300

    implementation homecomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export  const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+
